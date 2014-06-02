989d1eda7b3421727bca2a6c16b4db60bc97102d
walker <yangzizhe@gmail.com>
2014-06-03 07:42:02 +0800

    add chapter4

---
+++ linux/apue/chapter4/check_file_type.c
@@ -0,0 +1,23 @@
+#include "apue.h"
+
+int main(int argc, char *argv[])
+{
+    int i;
+    strcut stat buf;
+    char *ptr;
+
+    for (i = 1; i < argc; i++) {
+        printf("%s: ", argv[i]);
+        if (lstat(argv[i], &buf) < 0) {
+            err_ret("lstat error");
+            continue;
+        }
+        if (S_ISREG(buf.st_mode)) {
+            ptr = "regular";
+        }else if (S_ISDIR(buf.st_mode)) {
+            ptr = "directory";
+        }else if (S_ISCHR(buf.st_mode)) {
+            ptr = ""
+        }
+    }
+}
