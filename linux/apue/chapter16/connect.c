64f04cfa36de7bd87dc676ae3b74389eb858ee37
Walker Young <walkeryoung01@gmail.com>
2014-10-14 22:36:15 +0800

    add connect

--- linux/apue/chapter16/connect.c
+++ linux/apue/chapter16/connect.c
@@ -7,7 +7,7 @@ int connect_retry(int sockfd, const struct sockaddr *addr, socklen_t alen)
 {
     int nsec;
 
-    for (nsec = 1; nsec <= MAXSLEEP; nesc <<=1) {
+    for (nsec = 1; nsec <= MAXSLEEP; nsec <<=1) {
         if (connect(sockfd, addr, alen) == 0) {
             return(0);
         }
